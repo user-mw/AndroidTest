e17bf4db6d1344bf0d214b7eff8744d71b1b32d0
Titan-mw <myprbus@gmail.com>
2018-10-09 18:43:45 +0200

    Using of DiffUtil added.

--- app/src/main/java/com/test/androidtest/screens/CurrenciesAdapter.java
+++ app/src/main/java/com/test/androidtest/screens/CurrenciesAdapter.java
@@ -1,6 +1,8 @@
 package com.test.androidtest.screens;
 
 import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.AsyncListDiffer;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +26,20 @@ public class CurrenciesAdapter extends RecyclerView.Adapter<CurrencyViewHolder> 
 
     private List<CurrencyItem> mCurrencyItems = new ArrayList<>();
 
+    private final AsyncListDiffer<CurrencyItem> mDiffer = new AsyncListDiffer<CurrencyItem>(this, DIFF_CALLBACK);
+
+    private final static DiffUtil.ItemCallback<CurrencyItem> DIFF_CALLBACK = new DiffUtil.ItemCallback<CurrencyItem>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull CurrencyItem currencyItem, @NonNull CurrencyItem t1) {
+            return currencyItem.getAbbreviation().equals(t1.getAbbreviation());
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull CurrencyItem currencyItem, @NonNull CurrencyItem t1) {
+            return currencyItem.getValue() == t1.getValue();
+        }
+    };
+
     @Inject
     public CurrenciesAdapter() {
 
@@ -40,7 +56,7 @@ public class CurrenciesAdapter extends RecyclerView.Adapter<CurrencyViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull CurrencyViewHolder currencyViewHolder, int i) {
-        currencyViewHolder.bind(mCurrencyItems.get(i), mItemClick, mOnItemChangeAmount);
+        currencyViewHolder.bind(mDiffer.getCurrentList().get(i), mItemClick, mOnItemChangeAmount);
     }
 
     @Override
@@ -50,24 +66,25 @@ public class CurrenciesAdapter extends RecyclerView.Adapter<CurrencyViewHolder> 
 
     @Override
     public int getItemCount() {
-        return mCurrencyItems.size();
+        return mDiffer.getCurrentList().size();
     }
 
     public void addData(List<CurrencyItem> items) {
-        if(items != null) {
-            int start = 0;
-
-            if(mCurrencyItems.size() > 0) {
-                if(mCurrencyItems.get(0).getAbbreviation().equals(items.get(0).getAbbreviation())) {
-                    start = 1;
-                }
-
-                mCurrencyItems.clear();
-            }
-
-            mCurrencyItems.addAll(items);
-            notifyItemRangeChanged(start, items.size());
-        }
+//        if(items != null) {
+//            int start = 0;
+//
+//            if(mCurrencyItems.size() > 0) {
+//                if(mCurrencyItems.get(0).getAbbreviation().equals(items.get(0).getAbbreviation())) {
+//                    start = 1;
+//                }
+//
+//                mCurrencyItems.clear();
+//            }
+//
+//            mCurrencyItems.addAll(items);
+//            notifyItemRangeChanged(start, items.size());
+//        }
+        mDiffer.submitList(items);
     }
 
     public interface IOnItemClick {
